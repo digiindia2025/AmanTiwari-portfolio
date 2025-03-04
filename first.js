ab281d6c6e0b2811ed823a3d4b964aa9c42dc918
Aman Tiwari <amantiwariat2005@gmail.com>
2025-03-04 21:44:19 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,17 @@
+accountid = 144553
+let accountemail = "amantiwari5255@gmamil.com"
+var accountpassword = "12233179"
+accountcity = "mohaniya"
+let accountstate = ""
+
+//accountid= 2 // not allowed
+accountemail = "hc@hcgmail.com"
+accountpassword = "11223344"
+accountcity = "bhabua"
+accountstate = "bihar"
+
+console.log(accountid);
+
+
+console.table([accountid, accountemail, accountpassword, accountcity, accountstate])
+
